51ebfaad02347fb8145fec55fd89e65527b54c12
luiizhb <contatoprofissionallhb@gmail.com>
2023-02-15 23:29:26 +0000

    arquivo alterado na branch develop

--- gasolina.py
+++ gasolina.py
@@ -4,6 +4,6 @@ import seaborn as sns
 df = pd.read_csv('gasolina.csv')
 df
 sns.axes_style('whitegrid')
-grafico = sns.lineplot(data=df,x='dia',y='venda')
+grafico = sns.barplot(data=df,x='dia',y='venda')
 grafico.set(title='Grafico mostrando o valor da gasolina por dia',xlabel='Dia',ylabel='Preço')
 grafico.figure.savefig('grafico.png')
